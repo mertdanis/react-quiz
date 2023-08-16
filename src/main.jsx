c211a22c96b3d7d057becabc47ae3a1bd51a3309
M. D <mertdanis@outlook.com>
2023-08-16 11:15:17 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import { MainContext } from "./contexts/MainContext";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <MainContext>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-  </MainContext>
-
-  /* // </React.StrictMode>, */
+  </React.StrictMode>
 );
+
+reportWebVitals();
